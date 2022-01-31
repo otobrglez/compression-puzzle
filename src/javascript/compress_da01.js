f13bf14c6e668e408747db8054b5709327c6e2ad
davision <david@artnetik.si>
2022-01-31 11:40:40 +0100

    Update solutions and add solution with regex

--- src/javascript/compress_da01.js
+++ src/javascript/compress_da01.js
@@ -3,20 +3,19 @@ const assert = require('assert');
 const compress = (input) => {
   let store = ''
   let count = 1
-  let first = ''
-  input.split('').reduce((prev, cur) => {
-    if (cur === prev  ) {
+  input.split('').reduce((acc, cur, i, initial) => {
+    if (cur === initial[i+1] ) {
       count++
     } else {
-      store += count+cur
+      store += `${count}${cur}`
       count = 1
     }
     return cur
   }, '')
   return store
-}
+};
 
 assert.equal(
   compress("AAABBAAC"),
   "3A2B2A1C"
-)
+);
--- src/javascript/compress_da02.js
+++ src/javascript/compress_da02.js
@@ -1,22 +1,21 @@
 const assert = require('assert');
 
 const compress = (input) => {
-  let store = ''
-  let count = 1
-  let first = ''
-  input.split('').reduce((prev, cur, i, initial) => {
-    if (cur === initial[i+1] ) {
-      count++
-    } else {
-      store += count+cur
-      count = 1
-    }
-    return cur
-  }, '')
-  return store
-};
+	let r = ''
+	let c = 1
+  
+  input.split('').forEach((l, i, a) => {
+    if (l === a[i+1] ) {
+			c++
+		} else {
+		  r += `${c}${l}`
+		  c = 1
+		}
+  })
+	return r
+}
 
 assert.equal(
   compress("AAABBAAC"),
   "3A2B2A1C"
-);
+)
