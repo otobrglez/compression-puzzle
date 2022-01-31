c50dafd0186cb45c431121de5d1f0bd82fc953c6
David Ličen <david@artnetik.si>
2022-01-31 13:51:29 +0100

    Update compress_da02.js

--- src/javascript/compress_da02.js
+++ src/javascript/compress_da02.js
@@ -1,19 +1,19 @@
 const assert = require('assert');
 
 const compress = (input) => {
-	let r = ''
-	let c = 1
-  
+  let r = '';
+	let c = 1;
+
   input.split('').forEach((l, i, a) => {
     if (l === a[i+1] ) {
-			c++
+      c++;
 		} else {
-		  r += `${c}${l}`
-		  c = 1
+      r += `${c}${l}`;
+      c = 1;
 		}
-  })
-	return r
-}
+  });
+  return r;
+};
 
 assert.equal(
   compress("AAABBAAC"),
