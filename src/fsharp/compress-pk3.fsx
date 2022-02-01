d01ad6ca531bec2dc89293694a56cb82bc049941
Peter Kese <peter.kese@viidea.com>
2022-02-01 20:08:39 +0100

    Optimize imperative solution

--- src/fsharp/compress-pk3.fsx
+++ src/fsharp/compress-pk3.fsx
@@ -1,22 +1,17 @@
 #!/usr/bin/env -S dotnet fsi --define:DEBUG
 
-let compress = function
-    | "" -> ""
-    | s ->
-        use sw = new System.IO.StringWriter()
-        sw.GetStringBuilder().Capacity <- s.Length*2
-        let mutable lastCh = s[0]
-        let mutable reps = 0
-        for ch in s do
-            if ch = lastCh then
-                reps <- reps + 1
-            else
-                sw.Write(reps)
-                sw.Write(lastCh)
-                lastCh <- ch
-                reps <- 1
-        sw.Write(reps)
-        sw.Write(lastCh)
-        sw.ToString()
+open System
+
+let compress (s:string) =
+    use sw = new IO.StringWriter()
+    sw.GetStringBuilder().Capacity <- s.Length*2
+    let mutable s = s.AsSpan()
+    while not s.IsEmpty do
+        let mutable n = 1
+        while n<s.Length && s[n]=s[0] do n <- n+1
+        sw.Write(n)
+        sw.Write(s[0])
+        s <- s.Slice(n)
+    sw.ToString()
 
 assert(compress "AAABBAAC" = "3A2B2A1C")
