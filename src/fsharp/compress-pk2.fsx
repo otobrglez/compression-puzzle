6f98239e37cf93b8692b448bca25b46e1de11211
Peter Kese <peter.kese@viidea.com>
2022-02-01 13:54:50 +0100

    Add another F# solution

---
+++ src/fsharp/compress-pk2.fsx
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S dotnet fsi --define:DEBUG
+
+let rec compress = function
+    | "" -> ""
+    | s ->
+        let n = s |> Seq.takeWhile ((=) s[0]) |> Seq.length
+        $"{n}{s[0]}{compress s[n..]}"
+
+assert(compress "AAABBAAC" = "3A2B2A1Cx")
