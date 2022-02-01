295a6bcf698c440eb8973c454399c75ffb94fbbc
kristof_crnivec <kristof.crnivec@gmail.com>
2022-02-01 10:47:16 +0100

    Remove Minitest

--- src/ruby/compress-kbc-0.rb
+++ src/ruby/compress-kbc-0.rb
@@ -1,9 +1,5 @@
-require 'minitest/autorun'
-
 def compress(str)
   str.chars.chunk_while(&:==).map { "#{_1.size}#{_1.first}" }.join
 end
 
-describe 'test' do
-  it { assert_equal(compress('AAABBAAC'), '3A2B2A1C') }
-end
+raise 'Fail' unless compress('AAABBAAC') == '3A2B2A1C'
--- src/ruby/compress-kbc-1.rb
+++ src/ruby/compress-kbc-1.rb
@@ -1,9 +1,5 @@
-require 'minitest/autorun'
-
 def compress(str)
   str.chars.slice_when(&:!=).map { _1.tally.invert.to_a }.join
 end
 
-describe 'test' do
-  it { assert_equal(compress('AAABBAAC'), '3A2B2A1C') }
-end
+raise 'Fail' unless compress('AAABBAAC') == '3A2B2A1C'
