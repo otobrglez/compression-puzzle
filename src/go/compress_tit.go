48f509220abaed97b6482b69900c0329ef9af86a
Oto Brglez <otobrglez@gmail.com>
2022-02-07 11:12:21 +0100

    Fixing authors.

--- src/go/compress_tit.go
+++ src/go/compress_tit.go
@@ -1,33 +1,30 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
 
 func compress(in string) (out string) {
-	if len(in) == 0 {
-		return ""
-	}
-	ch := in[0]
-	count := 0
-	for {
-		if len(in) == 0 {
-			break
-		}
-		cur := in[0]
-		in = in[1:]
-		if cur == ch {
-			count++
-			continue
-		}
-		out = out + fmt.Sprintf("%d%s", count, string(ch))
-		ch = cur
-		count = 1
-	}
-	out = out + fmt.Sprintf("%d%s", count, string(ch))
-	return
+  if len(in) == 0 {
+    return ""
+  }
+  ch := rune(in[0])
+  count := 0
+  for _, cur := range in {
+    if cur == ch {
+      count++
+      continue
+    }
+    out = out + fmt.Sprint(count) + string(ch)
+    ch = cur
+    count = 1
+  }
+  out = out + fmt.Sprint(count) + string(ch)
+  return
 }
 
 func main() {
-	if compress("AAABBAAC") != "3A2B2A1C" {
-		panic("AAABBAAC does not match 3A2B2A1C")
-	}
+  if compress("AAABBAAC") != "3A2B2A1C" {
+    panic("AAABBAAC does not match 3A2B2A1C")
+  }
 }
