39ea0f98545578d5e3764bda13e6016a6632990c
Urban Škudnik <urban.skudnik@gmail.com>
2022-02-07 11:14:29 +0100

    Add benchmarks (#14)
    * Add file to benchmarks solutions
    
    * Benchmark with 10k executions, not 1M

---
+++ src/python/benchmarks.py
@@ -0,0 +1,48 @@
+import timeit
+
+from compress_brodul import compress as compress_brodul
+from compress_luka_faster import compress as compress_luka_faster
+from compress_luka import compress as compress_luka
+from compress_ob import compress as compress_ob
+from compress_urban import compress_iterative, compress_recursive
+
+number_of_executions = 10_000
+
+print(
+    "Brodul",
+    timeit.timeit(
+        "compress_brodul('AAABBAAC')", number=number_of_executions, globals=globals()
+    ),
+)
+print(
+    "Luka faster",
+    timeit.timeit(
+        "compress_luka_faster('AAABBAAC')",
+        number=number_of_executions,
+        globals=globals(),
+    ),
+)
+print(
+    "Luka",
+    timeit.timeit(
+        "compress_luka('AAABBAAC')", number=number_of_executions, globals=globals()
+    ),
+)
+print(
+    "Oto",
+    timeit.timeit(
+        "compress_ob('AAABBAAC')", number=number_of_executions, globals=globals()
+    ),
+)
+print(
+    "Urban, iterative",
+    timeit.timeit(
+        "compress_iterative('AAABBAAC')", number=number_of_executions, globals=globals()
+    ),
+)
+print(
+    "Urban, recursion",
+    timeit.timeit(
+        "compress_recursive('AAABBAAC')", number=number_of_executions, globals=globals()
+    ),
+)
