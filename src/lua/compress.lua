e491607d202d7ad4be8beb50bfb99127e6e9e5ec
smi <smi@eclipso.eu>
2022-03-05 10:06:29 +0100

    fixed typo and added few comments

--- src/lua/compress.lua
+++ src/lua/compress.lua
@@ -2,9 +2,9 @@ local function compress(s)
   local i, j = 1, 1
   local t = {}
   while i and i <= #s do
-    local c = s:sub(i,i)
-    i, j = s.find(s,c.."+",i)
-    if i then t[#t+1] = tostring(j-i+1)..c end
+    local c = s:sub(i,i) -- grab 1st char in sequence
+    i, j = s:find(c.."+",i) -- count repetitions
+    if i then t[#t+1] = tostring(j-i+1)..c end -- save sequence to table
     i = j + 1
   end
   return table.concat(t)
