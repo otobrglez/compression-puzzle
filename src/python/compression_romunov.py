db455c5aacd78109138d89363b736cce4f489259
Roman Luštrik <roman.lustrik@biolitika.si>
2022-04-06 21:14:15 +0200

    Add romunov's solution to Python benchmark

--- src/python/compression_romunov.py
+++ src/python/compression_romunov.py
@@ -2,27 +2,31 @@
 A very simple algorithm that iterates through the string and counts
 how many times a given character is detected.
 """
-input_string = "AAABBAAC"
 RESULT = "3A2B2A1C"
 
-output_string = []
 
-current_letter = input_string[0]
-current_counter = 0
+def compress_romunov(input_string):
+    output_string = []
 
-# Go through the string and record number of occurrences
-# per character.
-for el in input_string:
-    if el in current_letter:
-        current_counter += 1
-    else:
-        output_string.append(f"{current_counter}{current_letter}")
-        current_letter = el
-        current_counter = 1
+    current_letter = input_string[0]
+    current_counter = 0
 
-# Finally, add the last encoded character.
-output_string.append(f"{current_counter}{current_letter}")
+    # Go through the string and record number of occurrences
+    # per character.
+    for el in input_string:
+        if el in current_letter:
+            current_counter += 1
+        else:
+            output_string.append(f"{current_counter}{current_letter}")
+            current_letter = el
+            current_counter = 1
 
-output = "".join(output_string)
+    # Finally, add the last encoded character.
+    output_string.append(f"{current_counter}{current_letter}")
 
-assert output == RESULT
+    output = "".join(output_string)
+
+    return output
+
+
+assert compress_romunov(input_string="AAABBAAC") == RESULT
