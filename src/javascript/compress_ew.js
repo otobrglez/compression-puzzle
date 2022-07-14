1b94699ea778018ca0922fed8be192bb03f8ee9f
eran ws <eranws@gmail.com>
2022-07-14 10:55:17 +0300

    js reduce, 74 chars

--- src/javascript/compress_ew.js
+++ src/javascript/compress_ew.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 
 //     v 79 chars, no regex /\/\/\:
-compress=(s,r="",c=1)=>[...s].reduce((_,v,i,a)=>(v==a[i+1]?[0,c++]:[r+=c+v,c=1]),0)[0]
+compress=s=>[...s].reduce(([r,c],v,i,a)=>(v==a[i+1]?[r,c+1]:[r+c+v,1]),['',1])[0]
 
 assert.equal(compress("AAABBAAC"), "3A2B2A1C");
 
