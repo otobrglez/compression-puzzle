a307647d70c31631776b14006a10937b40a04054
eran ws <eranws@gmail.com>
2022-07-14 12:36:51 +0300

    map: 70 chars

--- src/javascript/compress_ew2.js
+++ src/javascript/compress_ew2.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 
-//     v 72 chars, no regex /\/\/\:
-compress=(s,r="",c=1)=>[...s].map((v,i,a)=>v==a[i+1]?c++:[r+=c+v,c=1]).pop()[0]
+//     v 70 chars, no regex /\/\/\:
+compress=(s,r="",c=1)=>[...s].map((v,i)=>v==s[i+1]?c++:[r+=c+v,c=1]).pop()[0]
 
 assert.equal(compress("AAABBAAC"), "3A2B2A1C");
