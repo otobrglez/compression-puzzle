794f95af401a781a3e214bdb1c54616be2e5f0db
Luka Prebil Grintal <luka.p.grintal@tretton37.com>
2022-07-14 11:47:38 +0200

    Decompress by chunking numbers + character (#69)

--- src/javascript/compress_lpg.js
+++ src/javascript/compress_lpg.js
@@ -1,3 +1,6 @@
 const compress = (s) => s.match(/(.)\1*/g).map(x => `${x.length}${x[0]}`).join('');
 
+const decompress = (s) => s.match(/(\d+)(.)/g).map(x => x[1].repeat(x[0])).join('');
+
 require("assert").equal(compress("AAABBAAC"), "3A2B2A1C");
+require("assert").equal(decompress("3A2B2A1C"), "AAABBAAC");
