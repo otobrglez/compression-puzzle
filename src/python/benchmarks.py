7fc37c99ca65b18b108d61283dae7e4ba442f25f
Teo Podobnik <teo.podobnik1234@gmail.com>
2022-10-20 17:19:39 +0200

    feat: compress_teo python compressor

--- src/python/benchmarks.py
+++ src/python/benchmarks.py
@@ -6,6 +6,7 @@ from compress_luka import compress as compress_luka
 from compress_ob import compress as compress_ob
 from compress_urban import compress_iterative, compress_recursive
 from compression_romunov import compress_romunov
+from compress_teo import compress_teo
 
 number_of_executions = 10_000
 
@@ -51,5 +52,11 @@ print(
     "romunov, babysteps",
     timeit.timeit(
         'compress_romunov("AAABBAAC")', number=number_of_executions, globals=globals()
+    )
+)
+print(
+    "Teo, babysteps",
+    timeit.timeit(
+        'compress_teo("AAABBAAC")', number=number_of_executions, globals=globals()
     )
 )
